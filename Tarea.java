f54d90748d05d2170813f6828ed8ee9eb70ee7db
Lucas Agutín Tolosa <97489178+Lucas-Tolosa@users.noreply.github.com>
2025-07-23 23:30:16 -0300

    Update Tarea.java

--- Tarea.java
+++ Tarea.java
@@ -4,40 +4,15 @@ import java.io.*;
 
 public class Tarea {
 
-	public Tarea(String descripcion,boolean completado) {
+	public Tarea(int id,String descripcion, boolean completado) {
+		this.id = id;
 		this.descripcion = descripcion;
 		this.completado = completado;
-		Tarea.id++;
-		this.tareaId=Tarea.id;
-		
 		
 	}
-	public void corroborarId() {
-		
-		try {
-			BufferedReader bf = new BufferedReader(new FileReader(new File("C:/Users/Lucas/Desktop/ListaTareas.in")));
-			int max = 0;
-			String linea = bf.readLine();
-			while(linea!=null) {
-				int proxLn = Integer.parseInt(linea);
-				bf.readLine();
-				bf.readLine();
-				if(proxLn>=max) {
-					max= proxLn;
-				}
-				linea= bf.readLine();
-			}
-			Tarea.id = max+1;
-			bf.close();
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
-		}
-		
+	
 	public int getId() {
-		return this.tareaId;
+		return this.id;
 	}
 	
 	public String getDescripcion() {
@@ -52,14 +27,12 @@ public class Tarea {
 
 	@Override
 	public String toString() {
-		return this.getId() + "\n" + this.getDescripcion() + "\n Completado:" + this.isCompletado()+"\n--";
+		return  this.getId()+"\n" + this.getDescripcion() + "\nCompletado:\n" + this.isCompletado()+"\n-----\n";
 		
 	}
 
-	private static int id = 0;
-	private int tareaId;
 	private String descripcion;
 	private boolean completado;
-	
+	private int id;
 	
 }
