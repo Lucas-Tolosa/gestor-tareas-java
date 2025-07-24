6ccb267272fc851592a97bb29136241d6cf3f19b
Lucas Agutín Tolosa <97489178+Lucas-Tolosa@users.noreply.github.com>
2025-07-23 23:29:58 -0300

    Update PruebaTarea.java

--- PruebaTarea.java
+++ PruebaTarea.java
@@ -1,38 +1,9 @@
 package pruebasG;
 
-import java.io.*;
+public class PruebaTarea {
 
-public class Tarea {
-
-	public Tarea(int id,String descripcion, boolean completado) {
-		this.id = id;
-		this.descripcion = descripcion;
-		this.completado = completado;
-		
-	}
-	
-	public int getId() {
-		return this.id;
-	}
-	
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public boolean isCompletado() {
-		return completado;
-	}
-	public void setCompletado(boolean completado) {
-		this.completado = completado;
-	}
-
-	@Override
-	public String toString() {
-		return  this.getId()+"\n" + this.getDescripcion() + "\nCompletado:\n" + this.isCompletado()+"\n-----\n";
-		
-	}
-
-	private String descripcion;
-	private boolean completado;
-	private int id;
-	
+public static void main(String[] args) {
+	GestionadorTareas gt = new GestionadorTareas();
+	gt.mostrarTareas();
+}
 }
